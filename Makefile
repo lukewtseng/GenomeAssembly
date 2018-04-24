5b80a32b32455bd6119d0eb57103e6ffc2b7bea1
latitude25 <luke4775@gmail.com>
2018-04-23 17:28:36 -0700

    Single node version

--- Makefile
+++ Makefile
@@ -9,11 +9,11 @@ OPENMP = -openmp #Note: this is the flag for Intel compilers. Change this to -fo
 CFLAGS = -g -std=c++11
 LIBS =
 
-TARGETS = mpi
+TARGETS = kmer_hash
 
-all:	$(TARGETS)
+all: $(TARGETS)
 
-mpi: kmer_mpi.cpp kmer_t.hpp pkmer_t.hpp packing.hpp read_kmers.hpp butil.hpp
+kmer_hash: kmer_mpi.cpp hashmap_mpi.hpp kmer_t.hpp pkmer_t.hpp packing.hpp read_kmers.hpp butil.hpp
 	$(MPCC) kmer_mpi.cpp -o $@ $(LIBS) $(MPILIBS) $(CFLAGS)
 
 
@@ -24,6 +24,16 @@ mpi.o: mpi.cpp common.h
 	$(MPCC) -c $(CFLAGS) mpi.cpp
 common.o: common.cpp common.h
 	$(CC) -c $(CFLAGS) common.cpp
+
+run:
+	salloc -N 1 -A mp309 -t 10:00 -q debug --qos=interactive -C haswell srun -N 1 -n 32 ./kmer_hash /global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/test.txt test
+
+runsmall:
+	salloc -N 1 -A mp309 -t 10:00 -q debug --qos=interactive -C haswell srun -N 1 -n 1 ./kmer_hash /global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/smaller/tiny.txt test
+
+check:
+	cat test*.dat | sort > my_solution.txt
+	diff my_solution.txt /global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/smaller/tiny_solution.txt
 
 clean:
 	rm -f *.o $(TARGETS) *.stdout *.txt
---
+++ hashmap_mpi.hpp
@@ -0,0 +1,92 @@
+#pragma once 
+
+#include <cstdio>
+#include <unordered_map>
+//#include <unordered_multimap>
+//using namespace std;
+
+struct mpi_hashmap {
+
+	// Global MPI metadata
+	size_t n_proc;
+	int rank;
+	
+	// hashtable metadata 
+	size_t total_size;
+	size_t local_size;
+
+	// Main data structure
+	std::unordered_multimap<uint64_t, kmer_pair> table;
+
+
+	// Constuctor
+	mpi_hashmap(size_t size);
+	
+	// Functions
+	bool insert(const kmer_pair &kmer);
+	bool find(const pkmer_t &key_kmer, kmer_pair &val_kmer);
+
+	size_t size();
+	void write_slot(uint64_t slot, const kmer_pair &kmer);
+	kmer_pair read_slot(uint64_t slot);
+
+	//bool request_slot(uint64_t slot);
+	//bool slot_used(uint64_t slot);
+
+};
+
+
+mpi_hashmap::mpi_hashmap(size_t size) {
+	table.reserve(size);
+}
+
+bool mpi_hashmap::insert(const kmer_pair &kmer) {
+	uint64_t hash = kmer.hash();
+	auto ret = table.insert(std::make_pair(hash, kmer));
+	return true;
+	/*
+  uint64_t probe = 0;
+	bool success = false;
+	do {
+		uint64_t slot = (hash + probe++) % size();
+		success = request_slot(slot);
+		if (success) {
+			write_slot(slot, kmer);
+		}
+	} while (!success && probe < size());
+	return success;
+	*/
+}
+
+bool mpi_hashmap::find(const pkmer_t &key_kmer, kmer_pair &val_kmer) {
+	uint64_t hash = key_kmer.hash();
+	
+	//typedef std::unordered_multimap<uint64_t, kmer_pair>::iterator MMAPIterator;
+	auto result = table.equal_range(hash);
+
+	for (auto it = result.first; it != result.second; it++) {
+		if (it->second.kmer == key_kmer) {
+			val_kmer = it->second;
+			return true;
+		}
+	}
+
+	return false;
+	/*
+	uint64_t probe = 0;
+	bool success = false;
+	do {
+		uint64_t slot = (hash + probe++) % size();
+		if (slot_used(slot)) {
+			val_kmer = read_slot(slot);
+			if (val_kmer.kmer == key_kmer) {
+				success = true;
+			}
+		}
+	} while (!success && probe < size());
+	return success;
+	*/
+}
+size_t mpi_hashmap::size() {
+	return table.size();
+}
